cd53ca3cfb2caab61e0bbb5d906c38e049fc8162
Mudassaralam <mudassar.alam.dev@gmail.com>
2025-06-08 14:22:17 +0500

    update: Issue fixed

--- src/pages/confirmBooking/ConfirmBooking.jsx
+++ src/pages/confirmBooking/ConfirmBooking.jsx
@@ -32,38 +32,56 @@ const ConfirmBooking = () => {
   };
 
   if (!user) {
-    return <p className="text-white">Loading user info...</p>;
+    return <p className='text-white'>Loading user info...</p>;
   }
 
   return (
-    <div className="p-6">
-      <table className="w-full text-left text-white border border-white">
+    <div className='p-6'>
+      <table className='w-full text-left text-white border border-white'>
         <thead>
-          <tr className="bg-gray-800">
-            <th className="p-3 border border-white">Field</th>
-            <th className="p-3 border border-white">Value</th>
+          <tr className='bg-gray-800'>
+            <th className='p-3 border border-white'>Field</th>
+            <th className='p-3 border border-white'>Value</th>
           </tr>
         </thead>
         <tbody>
-          <tr><td className="p-3 border border-white">First Name</td><td className="p-3 border border-white">{user.firstName}</td></tr>
-          <tr><td className="p-3 border border-white">Last Name</td><td className="p-3 border border-white">{user.lastName}</td></tr>
-          <tr><td className="p-3 border border-white">Email</td><td className="p-3 border border-white">{user.email}</td></tr>
-          <tr><td className="p-3 border border-white">Credits</td><td className="p-3 border border-white">{user.credits}</td></tr>
-          <tr><td className="p-3 border border-white">Login Count</td><td className="p-3 border border-white">{user.loginCount}</td></tr>
-          <tr><td className="p-3 border border-white">Last Login</td><td className="p-3 border border-white">{new Date(user.lastLogin).toLocaleString()}</td></tr>
+          <tr>
+            <td className='p-3 border border-white'>First Name</td>
+            <td className='p-3 border border-white'>{user.firstName}</td>
+          </tr>
+          <tr>
+            <td className='p-3 border border-white'>Last Name</td>
+            <td className='p-3 border border-white'>{user.lastName}</td>
+          </tr>
+          <tr>
+            <td className='p-3 border border-white'>Email</td>
+            <td className='p-3 border border-white'>{user.email}</td>
+          </tr>
+          <tr>
+            <td className='p-3 border border-white'>Credits</td>
+            <td className='p-3 border border-white'>{user.credits}</td>
+          </tr>
+          <tr>
+            <td className='p-3 border border-white'>Login Count</td>
+            <td className='p-3 border border-white'>{user.loginCount}</td>
+          </tr>
+          <tr>
+            <td className='p-3 border border-white'>Last Login</td>
+            <td className='p-3 border border-white'>{new Date(user.lastLogin).toLocaleString()}</td>
+          </tr>
         </tbody>
       </table>
 
-      <div className="mt-6 flex gap-4">
+      <div className='mt-6 flex gap-4'>
         <button
           onClick={handleAccept}
-          className="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700"
+          className='bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700'
         >
           Accept
         </button>
         <button
           onClick={handleReject}
-          className="bg-red-600 text-white px-6 py-2 rounded hover:bg-red-700"
+          className='bg-red-600 text-white px-6 py-2 rounded hover:bg-red-700'
         >
           Reject
         </button>
